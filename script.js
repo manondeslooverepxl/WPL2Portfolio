369dcfe3f416bcf0b14ed3ff58762444f102a892
manondeslooverepxl <manon.desloovere@student.pxl.be>
2023-06-11 19:15:17 +0200

    Versie 2

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+/* WPL PAGINA*/
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 
 /* Contact pagina */
 $(document).ready(function(){
