f40561d9779a67a6bd8f95ceb609c02ae5903d7d
manondeslooverepxl <manon.desloovere@student.pxl.be>
2023-06-11 16:41:17 +0200

    Versie 2

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-function toggleMenu() {
-    var menuBg = document.getElementById("menu-bg");
-    var lines = document.getElementsByClassName("line");
-
-    if (menuBg.style.opacity === "" || menuBg.style.opacity === "0") {
-        menuBg.style.opacity = "1";
-        menuBg.style.pointerEvents = "auto";
-        menuBg.style.transform = "scale(1)";
-        for (var i = 0; i < lines.length; i++) {
-            lines[i].classList.add("cross");
-        }
-    } else {
-        menuBg.style.opacity = "0";
-        menuBg.style.pointerEvents = "none";
-        menuBg.style.transform = "scale(0)";
-        for (var i = 0; i < lines.length; i++) {
-            lines[i].classList.remove("cross");
-        }
-    }
-}
-
 
 
 /* Contact pagina */
