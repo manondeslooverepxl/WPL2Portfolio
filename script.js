ba770992e8dafbc8c2acc0e1a4c81ccba4850679
manondeslooverepxl <manon.desloovere@student.pxl.be>
2023-06-11 20:29:32 +0200

    Versie 2

--- script.js
+++ script.js
@@ -15,11 +15,3 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-
-/* Contact pagina */
-$(document).ready(function(){
-    $("#vragen").find(".uitklap-trigger").on("click",function(){
-        $(".antwoord").slideUp();
-        $(this).next(".antwoord").slideDown();
-    });
-});
